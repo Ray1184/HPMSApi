7f378127081cff9909fad579c92be47b648d068e
Nick Ray <nickgreppi1184@gmail.com>
2021-01-24 17:34:01 +0100

    Api manager

--- include/api/HPMSActorAdapter.h
+++ include/api/HPMSActorAdapter.h
@@ -18,22 +18,22 @@ namespace hpms
             return "ActorAdapter";
         }
 
-        virtual const std::string GetName() const = 0;
+        virtual std::string GetName() = 0;
 
         virtual void SetPosition(const glm::vec3& position) = 0;
 
-        virtual const glm::vec3& GetPosition() const = 0;
+        virtual glm::vec3& GetPosition() = 0;
 
         virtual void SetRotation(const glm::quat& rotation) = 0;
 
-        virtual const glm::quat& GetRotation() const = 0;
+        virtual glm::quat& GetRotation() = 0;
 
         virtual void SetScale(const glm::vec3& scale) = 0;
 
-        virtual const glm::vec3& GetScale() const = 0;
+        virtual glm::vec3& GetScale() = 0;
 
         virtual void SetVisible(bool visible) = 0;
 
-        virtual bool IsVisible() const = 0;
+        virtual bool IsVisible() = 0;
     };
 }
